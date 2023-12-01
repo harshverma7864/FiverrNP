85bbbb660b0a67f5308de5e879515499c9950c10
navneet2026 <navneet@netpuppys.com>
2023-12-02 01:34:24 +0530

    footer pages are done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-
-//Import Pages 
 import HomePage from './pages/HomePage';
-
-
-
-
-
-
 
 function App() {
   return (
-    <Router>
     <>
-    <Routes>
-     <Route path="/" element={<HomePage/>}/>
-    </Routes>
-
+    <HomePage/>
     </>
-    </Router>
   );
 }
 

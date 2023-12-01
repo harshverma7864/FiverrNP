2243b4ec9e70bb660e02a97f54e4b94fcedadf83
harshverma7864 <harshverma7864@gmail.com>
2023-12-01 21:53:49 +0530

    Addded card components and scroll bar below banner on home page

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import cardStyle from '../assets/styles/card.module.css'
+import cardImage from  '../assets/images/cardimage1.png'
+import noImage from '../assets/images/cardimage2.png'
+const Card = ({heading , subHeadings}) => {
 
-const Card = () => {
   return (
-    <div>Card</div>
+    <div className={cardStyle.card}>
+      <div className={cardStyle.cardImage}>
+        <img src={cardImage} alt={noImage}/>
+      </div>
+      <h4>{heading}</h4>
+      {
+        subHeadings.map((item , index)=>(
+          <p key={index}>{item}</p>
+        ))
+      }
+    </div>
   )
 }
 
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,7 +3,58 @@ import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import style from '../assets/styles/homepage.module.css'
 import bannerImage from '../assets/images/bannerImage1.png'
+import Card from '../components/Card'
+
 const HomePage = () => {
+
+    var cardData = [
+        {
+            heading:"Logo and Brand Identity",
+            subHeading:["Logo Design" , "Brand Style Guide" , "Business Cards" , "Fonts and Typography"]
+        },
+        
+        {
+            heading:"Web and App Design",
+            subHeading:["Website Design" , "App Design" , "UX Design" , "Landing Page Design","Icon Design"]
+        },
+        
+        {
+            heading:"Art & Illustration",
+            subHeading:["Website Design" , "App Design" , "UX Design" , "Landing Page Design","Icon Design" ,"Icon Design" , "Landing Page Design" ,"Landing Page Design"]
+        },
+        {
+            heading:"Logo and Brand Identity",
+            subHeading:["Logo Design" , "Brand Style Guide" , "Business Cards" , "Fonts and Typography"]
+        },
+        
+        {
+            heading:"Web and App Design",
+            subHeading:["Website Design" , "App Design" , "UX Design" , "Landing Page Design","Icon Design"]
+        },
+        
+        {
+            heading:"Art & Illustration",
+            subHeading:["Website Design" , "App Design" , "UX Design" , "Landing Page Design","Icon Design" ,"Icon Design" , "Landing Page Design" ,"Landing Page Design"]
+        },
+        {
+            heading:"Logo and Brand Identity",
+            subHeading:["Logo Design" , "Brand Style Guide" , "Business Cards" , "Fonts and Typography"]
+        },
+        
+        {
+            heading:"Web and App Design",
+            subHeading:["Website Design" , "App Design" , "UX Design" , "Landing Page Design","Icon Design"]
+        },
+        
+        {
+            heading:"Art & Illustration",
+            subHeading:["Website Design" , "App Design" , "UX Design" , "Landing Page Design","Icon Design" ,"Icon Design" , "Landing Page Design" ,"Landing Page Design"]
+        },
+
+    ]
+    var mostPopular =["Image Editing" , "Social Media Design" , "Ai Artist" , "Web Design" , "Interior Design",
+    "Image Editing" , "Social Media Design" , "Ai Artist" , "Web Design" , "Interior Design"]
+
   return (
     <>
     <Navbar/>
@@ -24,8 +75,27 @@ const HomePage = () => {
         
     </div>
 
+    <div className={style.scrollView1}>
+      <h1 className={style.heading}>Most Popular Graphics and Design</h1>
+      <div className={style.scrollViewInner}>
+        {mostPopular.map((item, index) => (
+          <div key={index} className={style.scrollViewItem}>
+            {item} <span style={{color:'#F04C43'}}>&rarr;</span>
+          </div>
+        ))}
+      </div>
+    </div>
 
+    <div className={style.serviceCardsOuter}>
+    <h1 className={style.heading}>Explore Graphics and Design</h1> 
+    <div className={style.serviceCards}>
+    {
+        cardData.map((item)=>(<Card heading={item.heading} subHeadings={item.subHeading}/>))
+    }
+    </div>
+    </div>
 
+  
     <Footer/>
     </>
   )

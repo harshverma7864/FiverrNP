1aacf8af418683cbb294a5bdbc5b714e37424aea
harshverma7864 <harshverma7864@gmail.com>
2023-12-13 00:35:39 +0530

    Added condition in navbar category dropdown

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -82,14 +82,16 @@ const Navbar = () => {
                 categoryData.map((item, index) => (
                   <li key={index} onClick={(event) => handleClick2(event, item.id)}>
                     {item.category_name}
-                    {
+                    {  item.subcategory.length > 0 ?      (
                       item.subcategory.length > 0 &&
                     isClickedCategory2 === item.id ? (<svg xmlns="http://www.w3.org/2000/svg" width="14" height="6" viewBox="0 0 14 6" fill="none">
                       <path fill-rule="evenodd" clip-rule="evenodd" d="M12.5 6L7 1.69565L1.5 6L2.28059e-08 5.47826L7 -1.70857e-07L14 5.47826L12.5 6Z" fill="#F04C43"/>
                       </svg>) : (<svg xmlns="http://www.w3.org/2000/svg" width="14" height="6" viewBox="0 0 14 6" fill="none">
                       <path fill-rule="evenodd" clip-rule="evenodd" d="M1.5 -5.46392e-07L7 4.30435L12.5 -6.55671e-08L14 0.521739L7 6L-2.28059e-08 0.521739L1.5 -5.46392e-07Z" fill="black"/>
                     </svg>)
-                    }
+                    ): ('')
+              }
+
                   </li>
                 ))}
             </ul>
@@ -103,14 +105,16 @@ const Navbar = () => {
                   <li onClick={(ev) => handleClick3(ev, item2.id)} key={index2}>
                     {item2.subcategory_name}
 
-                    {
-                      item2.subsubcategory.length > 0 &&
+               {  item2.subsubcategory.length > 0 ?    (
+                      
                     isClickedCategory3 === item2.id ? (<svg xmlns="http://www.w3.org/2000/svg" width="14" height="6" viewBox="0 0 14 6" fill="none">
                       <path fill-rule="evenodd" clip-rule="evenodd" d="M12.5 6L7 1.69565L1.5 6L2.28059e-08 5.47826L7 -1.70857e-07L14 5.47826L12.5 6Z" fill="#F04C43"/>
                       </svg>) : (<svg xmlns="http://www.w3.org/2000/svg" width="14" height="6" viewBox="0 0 14 6" fill="none">
                       <path fill-rule="evenodd" clip-rule="evenodd" d="M1.5 -5.46392e-07L7 4.30435L12.5 -6.55671e-08L14 0.521739L7 6L-2.28059e-08 0.521739L1.5 -5.46392e-07Z" fill="black"/>
                     </svg>)
-                    }
+                    ) : ('')
+
+              }
                     
                   </li>
                 ))}

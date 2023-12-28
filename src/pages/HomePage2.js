462c12a850614067bec0f832a99f38716ea43001
harshverma7864 <harshverma7864@gmail.com>
2023-12-28 23:29:06 +0530

    Fixed some routing glitches

--- src/pages/HomePage2.js
+++ src/pages/HomePage2.js
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import style from '../assets/styles/homepage.module.css'
 import { Navbar2 } from '../components'
 import bannerImage from '../assets/images/bannerImage1.png'
+import Cookies from 'js-cookie'
+import { useHistory } from 'react-router-dom'
 
 
 const HomePage2 = () => {
+ 
+  const history = useHistory();
+
+  useEffect(() => {
+
+    const userDetailCookie = Cookies.get("user");
+    const isLoggedIn = localStorage.getItem('loggedin');
+
+
+    if (isLoggedIn && userDetailCookie) {
+      const userDetail = JSON.parse(userDetailCookie);
+    // Check if the user is logged in by looking at the 'loggedin' item in localStorage
+    const isLoggedIn = localStorage.getItem('loggedin');
+
+    // If not logged in, redirect to the home page or another page of your choice
+    if (isLoggedIn && userDetail.user_type === "internal_manager") {
+      history.push('/requests');  // You can replace '/' with the path of the page where you want to redirect the user
+    }else{
+      history.push("/dashboard")
+    }}
+    else{
+      history.push("/")
+    }
+  }, [history]);
+
+
+
     var mostPopular =["Image Editing" , "Social Media Design" , "Ai Artist" , "Web Design" , "Interior Design",
     "Image Editing" , "Social Media Design" , "Ai Artist" , "Web Design" , "Interior Design"]
 
